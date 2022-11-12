df95cb5d8cfdbc630569aedf1a344ca99dfcf0f0
Fang <neulf@hotmail.com>
2022-11-13 00:29:38 +0800

    add code file.

---
+++ pygpg.py
@@ -0,0 +1,17 @@
+﻿import os
+
+def check_file(file_path):
+    os.chdir(file_path)
+    print(os.path.abspath(os.curdir))
+    all_file = os.listdir()
+    files = []
+    for f in all_file:
+        if os.path.isdir(f):
+            files.extend(check_file(file_path+'/'+f))
+            os.chdir(file_path)
+        else:
+            files.append(f)
+            print(f)
+    #return files
+
+file_list = check_file(".")
