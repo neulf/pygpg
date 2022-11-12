7d252cc3af83fb0d6242c4f073b7ce029eb86db7
Fang <neulf@hotmail.com>
2022-11-13 01:53:53 +0800

    updated.

--- pygpg.py
+++ pygpg.py
@@ -1,17 +1,30 @@
 ﻿import os
 
-def check_file(file_path):
+def check_file(file_path, reverse=False):
+    if(file_path.find(".") == 0 and reverse == True):
+        return
+        
+    #print("->"+file_path)
     os.chdir(file_path)
-    print(os.path.abspath(os.curdir))
+    #print(os.path.abspath(os.curdir))
     all_file = os.listdir()
     files = []
-    for f in all_file:
-        if os.path.isdir(f):
-            files.extend(check_file(file_path+'/'+f))
-            os.chdir(file_path)
-        else:
-            files.append(f)
-            print(f)
+    
+    if all_file:
+        for f in all_file:
+            if os.path.isdir(f):
+                
+                if(f.find(".") == 0):
+                    continue
+                                                 
+                #print(type(f))
+                check_file(file_path+'/'+f,True)
+                os.chdir(file_path)
+            else:
+                if(f.find(".") == 0):
+                    continue
+                #files.append(f)
+                print("--->"+f)
     #return files
 
 file_list = check_file(".")
